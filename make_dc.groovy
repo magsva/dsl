fae80bcb07db8357721e3953df14ad79462fce3f
Magnus Svantesson <magnus.svantesson@ericsson.com>
2014-09-05 09:08:07 +0200

    New file

---
+++ make_dc.groovy
@@ -0,0 +1,15 @@
+job {
+apapa
+    name 'DSL-Tutorial-1-Test'
+    scm {
+        git('git://github.com/jgritman/aws-sdk-test.git')
+    }
+    triggers {
+        scm('*/15 * * * *')
+    }
+    steps {
+        maven('-e clean test')
+    }
+}
+
+
